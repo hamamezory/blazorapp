cf4e09447d421cadaa37686b8c61f300216ddea7
hama <hamamezory@gmail.com>
2021-08-04 22:07:52 +0300

    Implement Authorization infra structure

--- src/apptest1/Program.cs
+++ src/apptest1/Program.cs
@@ -1,4 +1,5 @@
 using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,6 +29,8 @@ namespace apptest1
             builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("apptest1.Api"));
             builder.Services.AddMudServices();
             builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
             await builder.Build().RunAsync();
         }
     }
